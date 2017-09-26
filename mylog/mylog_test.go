f0d149fb93d8d248b655287aade43a1bfb919637
tk103331 <tk103331@gmail.com>
2017-09-26 23:44:44 +0800

    add test case:TestSetOutput,TestSetLevel

--- mylog/mylog_test.go
+++ mylog/mylog_test.go
@@ -1,13 +1,43 @@
 package mylog
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
 const (
-	default_prefix = "  %s  "
+	default_prefix = "testing: %s"
 )
 
 func TestStdLog(t *testing.T) {
-	std.Info(t.Name())
+	std.Info(fmt.Sprintf(default_prefix, t.Name()))
+}
+
+func TestSetOutput(t *testing.T) {
+	w, err := os.Create("test.log")
+	if err != nil {
+		fmt.Printf("open file error:%v\n", err)
+	}
+	l := New(w, default_log_format, LevelInfo)
+	l.Infof(default_prefix, t.Name())
+	l.Info("test")
+}
+
+func TestSetFormat(t *testing.T) {
+	format := "#time 在 #file 文件 #line 行，输出：#msg"
+	l := New(os.Stdout, format, LevelInfo)
+	l.Infof(default_prefix, t.Name())
+	l.Info("test")
+}
+
+func TestSetLevel(t *testing.T) {
+	l := New(os.Stdout, default_log_format, LevelInfo)
+	l.Warnf(default_prefix, t.Name())
+	l.Trace("trace log")
+	l.Debug("debug log")
+	l.Info("info log")
+	l.Warn("warn log")
+	l.Error("error log")
+	l.Fatal("fatal log")
 }
