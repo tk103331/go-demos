f01a86594da079a177b2f4814ca3fb8245c9c803
tk103331 <tk103331@gmail.com>
2019-11-24 23:26:17 +0800

    request

--- echodemo/go.mod
+++ echodemo/go.mod
@@ -4,13 +4,15 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.10 // indirect
 	github.com/valyala/fasttemplate v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20191117063200-497ca9f6d64f // indirect
 	golang.org/x/net v0.0.0-20191116160921-f9c825593386 // indirect
 	golang.org/x/sys v0.0.0-20191118073850-2f86c98f3482 // indirect
-	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/go-playground/validator.v9 v9.30.0
 )
--- echodemo/routes/request.go
+++ echodemo/routes/request.go
@@ -4,16 +4,64 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"gopkg.in/go-playground/validator.v9"
 )
 
-func registerRequestRoute(e *echo.Echo) {
+type CustomBinder struct {}
 
-	// Handler
+func (cb *CustomBinder) Bind(i interface{}, c echo.Context) (err error) {
+	// You may use default binder
+	db := new(echo.DefaultBinder)
+	if err = db.Bind(i, c); err != echo.ErrUnsupportedMediaType {
+		return
+	}
+
+	// Define your custom implementation
+
+	return
+}
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
+
+func registerRequestRoute(e *echo.Echo) {
+	// Custom Binder
+	e.Binder = &CustomBinder{}
+
+	// Validate Data
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	// Bind Data
+	// To bind request body into a Go type use Context#Bind(i interface{}). The default binder supports decoding application/json, application/xml and application/x-www-form-urlencoded data based on the Content-Type header.
 	e.POST("/user/create", func(c echo.Context) (err error) {
 		u := new(User)
 		if err = c.Bind(u); err != nil {
 			return
 		}
 		return c.JSON(http.StatusOK, u)
+	})
+
+	// Form Data
+	e.POST("/user/create2", func(c echo.Context) error {
+		name := c.FormValue("name")
+		return c.String(http.StatusOK, name)
+	})
+
+	// Query Parameters
+	e.GET("/user/byname", func(c echo.Context) error {
+		name := c.QueryParam("name")
+		return c.String(http.StatusOK, name)
+	})
+
+	// Path Parameters
+	e.GET("/user/:name", func(c echo.Context) error {
+		name := c.Param("name")
+		return c.String(http.StatusOK, name)
 	})
 }
